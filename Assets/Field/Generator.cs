4193c6bae0f484b97d6a002487ed4fe1c34ef22a
SatouTappei <stutpi.signup@gmail.com>
2022-12-01 10:52:45 +0900

    背景、ジェネレータ、魚のプレハブを作成

---
+++ Assets/Field/Generator.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 魚を生成するコンポーネント
+/// </summary>
+public class Generator : MonoBehaviour
+{
+    [SerializeField] GameObject _prefabs;
+    [SerializeField] float _distance;
+
+    [Header("ジェネレータの稼働と停止を切り替える")]
+    public bool IsActive = false;
+
+    IEnumerator Start()
+    {
+        // 外部から稼働されるまで待つ
+        yield return new WaitUntil(() => IsActive);
+
+        while (IsActive)
+        {
+            // 生成もしくはオブジェクトプールから取る
+            yield return new WaitForSeconds(_distance);
+        }
+        Debug.Log("ジェネレータ停止");
+    }
+
+    void Update()
+    {
+        
+    }
+}
