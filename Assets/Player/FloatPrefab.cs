ec6150d746820c6cf41b028328df82f4bf21e788
SatouTappei <stutpi.signup@gmail.com>
2022-12-02 10:34:06 +0900

    クリックした位置に飛ばす処理を追加

--- Assets/Player/FloatPrefab.cs
+++ Assets/Player/FloatPrefab.cs
@@ -2,27 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using DG.Tweening;
 
 /// <summary>
 /// 浮き輪の制御を行うコンポーネント
 /// </summary>
 public class FloatPrefab : MonoBehaviour
 {
+    [SerializeField] Collider2D _col;
+
+    Tween _tween;
+
     void Start()
     {
-        
+        Move();
     }
 
     void Update()
     {
+        // テスト用
+        //if (Input.GetKeyDown(KeyCode.D)) Destroy(gameObject);
+    }
 
+    /// <summary>マウスカーソルの位置に移動させる</summary>
+    void Move()
+    {
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        pos.z = 0;
+        _tween = transform.DOMove(pos, 0.5f);
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
+        // 2匹以上ヒットしないように判定を消す
+        _col.enabled = false;
+
         ExecuteEvents.Execute<IFloatHitable>(collision.gameObject, null, (reciever, _) =>
         {
             reciever.HitReceived();
         });
+    }
+
+    void OnDestroy()
+    {
+        _tween.Kill();
     }
 }
--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -17,18 +17,29 @@ public class PlayerController : MonoBehaviour
     {
         Init();
 
-        // 予め生成しておく
-        GameObject go = Instantiate(_floatPrefab, _floatPivot.position, Quaternion.identity);
+        while (true)
+        {
+            // マウスの位置に浮きを投げる
+            yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
 
-        // マウスの位置に浮きを投げる
-        yield return new WaitUntil(()=>Input.GetMouseButtonDown(0));
-        Vector3 pos = _camera.ScreenToWorldPoint(Input.mousePosition);
-        pos.z = 0;
+            // 生成
+            GameObject go = Instantiate(_floatPrefab, _floatPivot.position, Quaternion.identity);
 
-        // クリックされたらその位置に飛ばす
-        go.transform.DOMove(pos, 0.5f);
+            //Vector3 pos = _camera.ScreenToWorldPoint(Input.mousePosition);
+            //pos.z = 0;
 
+            // クリックされたらその位置に飛ばす
+            //Tween t = go.transform.DOMove(pos, 0.5f);
+            // 指定位置まで来たらコライダーをオンにする
+
+            yield return new WaitUntil(() => go == null);
+
+            //t.Kill();
+            Debug.Log("浮きが消えました");
+        }
         // 浮きの感知範囲に魚が入ったら釣りバトル開始
+        // インターフェースで実装
+
         // スペースキー連打でバトルする
         // 毎フレームカウントが下がり続け、0になったら敗北
         // 勝てば魚ゲット、カウントアップ
